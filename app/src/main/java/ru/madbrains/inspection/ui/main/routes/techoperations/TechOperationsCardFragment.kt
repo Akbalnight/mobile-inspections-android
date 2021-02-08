6fb57194b9843d01c5d589195cb4e395bc72a5eb
Aleksey Polesov <a.polesov@madbrains.ru>
2021-02-08 13:47:29 +0400

    [15] add button defect

--- app/src/main/java/ru/madbrains/inspection/ui/main/routes/techoperations/TechOperationsCardFragment.kt
+++ app/src/main/java/ru/madbrains/inspection/ui/main/routes/techoperations/TechOperationsCardFragment.kt
@@ -2,14 +2,18 @@ package ru.madbrains.inspection.ui.main.routes.techoperations
 
 import android.os.Bundle
 import androidx.core.view.isVisible
+import androidx.lifecycle.Observer
 import kotlinx.android.synthetic.main.fragment_route_list.*
+import kotlinx.android.synthetic.main.fragment_tech_operations_card.*
+import kotlinx.android.synthetic.main.toolbar_with_menu.*
 import org.koin.androidx.viewmodel.ext.android.sharedViewModel
 import org.koin.androidx.viewmodel.ext.android.viewModel
 import ru.madbrains.inspection.R
+
 import ru.madbrains.inspection.base.BaseFragment
-import ru.madbrains.inspection.ui.adapters.RouteAdapter
+import ru.madbrains.inspection.extensions.strings
+import ru.madbrains.inspection.ui.main.routes.RoutesAdapter
 import ru.madbrains.inspection.ui.main.routes.RoutesViewModel
-import ru.madbrains.inspection.ui.main.routes.routelist.RouteListViewModel
 
 
 class TechOperationsCardFragment : BaseFragment(R.layout.fragment_tech_operations_card) {
@@ -17,11 +21,18 @@ class TechOperationsCardFragment : BaseFragment(R.layout.fragment_tech_operation
     private val techOperationsCardViewModel: TechOperationsCardViewModel by viewModel()
     private val routesViewModel: RoutesViewModel by sharedViewModel()
 
-
+    private lateinit var techOperationsAdapter: TechOperationsAdapter
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
 
+        setupToolbar()
+
+        techOperationsCardViewModel.getCard()
+
+        techOperationsCardViewModel.techOperations.observe(viewLifecycleOwner, Observer {
+            techOperationsAdapter.items = it
+        })
       /*  rvRoutes.adapter = routesAdapter
 
         btnGetData.setOnClickListener {
@@ -33,5 +44,11 @@ class TechOperationsCardFragment : BaseFragment(R.layout.fragment_tech_operation
             ivGetData.isVisible = false
             btnGetData.isVisible = false
         })*/
+    }
+
+    private fun setupToolbar() {
+        toolbarLayout.apply {
+            tvTitle.text = strings[R.string.fragment_sync_title]
+        }
     }
 }
--- app/src/main/java/ru/madbrains/inspection/ui/main/routes/techoperations/TechOperationsCardViewModel.kt
+++ app/src/main/java/ru/madbrains/inspection/ui/main/routes/techoperations/TechOperationsCardViewModel.kt
@@ -1,11 +1,41 @@
 package ru.madbrains.inspection.ui.main.routes.techoperations
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.rxkotlin.addTo
 import ru.madbrains.domain.interactor.RoutesInteractor
+import ru.madbrains.domain.model.PlanTechOperationsModel
+import ru.madbrains.domain.model.RouteModel
 import ru.madbrains.inspection.base.BaseViewModel
+import ru.madbrains.inspection.base.model.DiffItem
 
-class TechOperationsCardViewModel(private val routesInteractor: RoutesInteractor) : BaseViewModel() {
+class TechOperationsCardViewModel(private val routesInteractor: RoutesInteractor) :
+    BaseViewModel() {
+
+    private val _progressVisibility = MutableLiveData<Boolean>()
+    val progressVisibility: LiveData<Boolean> = _progressVisibility
+
+    private val _techOperations = MutableLiveData<List<DiffItem>>()
+    val techOperations: LiveData<List<DiffItem>> = _techOperations
+
+    private val operationsModels = mutableListOf<PlanTechOperationsModel>()
 
     fun routeClick() {
 
+    }
+
+    fun getCard() {
+        routesInteractor.getPlanTechOperations()
+            .observeOn(AndroidSchedulers.mainThread())
+            .doOnSubscribe { _progressVisibility.postValue(true) }
+            .doAfterTerminate { _progressVisibility.postValue(false) }
+            .subscribe({ operations ->
+                operationsModels.addAll(operations)
+              //  updateData()
+            }, {
+                it.printStackTrace()
+            })
+            .addTo(disposables)
     }
 }
