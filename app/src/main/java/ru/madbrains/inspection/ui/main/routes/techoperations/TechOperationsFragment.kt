f77467ec2400ac52dfa73e36062d0acfceb8729c
Aleksey Polesov <a.polesov@madbrains.ru>
2021-02-09 15:42:53 +0400

    [15] change ui

--- app/src/main/java/ru/madbrains/inspection/ui/main/routes/techoperations/TechOperationsFragment.kt
+++ app/src/main/java/ru/madbrains/inspection/ui/main/routes/techoperations/TechOperationsFragment.kt
@@ -3,21 +3,18 @@ package ru.madbrains.inspection.ui.main.routes.techoperations
 import android.os.Bundle
 import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
-import kotlinx.android.synthetic.main.fragment_route_points.*
 import kotlinx.android.synthetic.main.fragment_tech_operations.*
 import kotlinx.android.synthetic.main.fragment_tech_operations.progressView
 import kotlinx.android.synthetic.main.fragment_tech_operations.toolbarLayout
 import kotlinx.android.synthetic.main.toolbar_with_back.view.*
 import kotlinx.android.synthetic.main.toolbar_with_close.view.tvTitle
 import org.koin.androidx.viewmodel.ext.android.viewModel
-import ru.madbrains.domain.model.RouteModel
 import ru.madbrains.domain.model.RoutePointModel
 import ru.madbrains.inspection.R
 
 import ru.madbrains.inspection.base.BaseFragment
 import ru.madbrains.inspection.extensions.strings
 import ru.madbrains.inspection.ui.adapters.TechOperationAdapter
-import ru.madbrains.inspection.ui.main.routes.points.RoutePointsFragment
 
 
 class TechOperationsFragment : BaseFragment(R.layout.fragment_tech_operations) {
@@ -26,7 +23,7 @@ class TechOperationsFragment : BaseFragment(R.layout.fragment_tech_operations) {
         const val KEY_POINT = "point"
     }
 
-    private val techOperationsCardViewModel: TechOperationsViewModel by viewModel()
+    private val techOperationsViewModel: TechOperationsViewModel by viewModel()
 
     private val techOperationsAdapter by lazy {
         TechOperationAdapter()
@@ -35,23 +32,24 @@ class TechOperationsFragment : BaseFragment(R.layout.fragment_tech_operations) {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
 
-        techOperationsCardViewModel.progressVisibility.observe(viewLifecycleOwner, Observer {
+        techOperationsViewModel.progressVisibility.observe(viewLifecycleOwner, Observer {
             progressView.changeVisibility(it)
         })
 
         requireNotNull(arguments).run {
             (getSerializable(TechOperationsFragment.KEY_POINT) as? RoutePointModel)?.let {
                 setupToolbar(it.position)
-                techOperationsCardViewModel.setPoint(it)
+                techOperationsViewModel.setPoint(it)
             }
         }
 
         rvTechOperations.adapter = techOperationsAdapter
 
-        techOperationsCardViewModel.titleTechOperations.observe(viewLifecycleOwner, Observer {
+        techOperationsViewModel.titleTechOperations.observe(viewLifecycleOwner, Observer {
             tvTitleTechOperations.text = it
         })
-        techOperationsCardViewModel.techOperations.observe(viewLifecycleOwner, Observer {
+
+        techOperationsViewModel.techOperations.observe(viewLifecycleOwner, Observer {
             techOperationsAdapter.items = it
         })
 
