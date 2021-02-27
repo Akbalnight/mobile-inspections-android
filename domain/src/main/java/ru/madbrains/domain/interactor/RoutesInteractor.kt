929b3e534c5a23dd274a30630fd6d7b197fad3f3
Aleksey Polesov <a.polesov@madbrains.ru>
2021-02-27 13:15:34 +0400

    [18] add defect domain

--- domain/src/main/java/ru/madbrains/domain/interactor/RoutesInteractor.kt
+++ domain/src/main/java/ru/madbrains/domain/interactor/RoutesInteractor.kt
@@ -6,21 +6,21 @@ import ru.madbrains.domain.model.*
 import ru.madbrains.domain.repository.RoutesRepository
 
 class RoutesInteractor(
-    private val routesRepository: RoutesRepository
+        private val routesRepository: RoutesRepository
 ) {
     fun getRoutes(): Single<List<RouteModel>> {
         return routesRepository.getRoutes()
-            .subscribeOn(Schedulers.io())
+                .subscribeOn(Schedulers.io())
     }
 
     fun getRoutePoints(routeId: String): Single<List<RoutePointModel>> {
         return routesRepository.getRoutePoints(routeId)
-            .subscribeOn(Schedulers.io())
+                .subscribeOn(Schedulers.io())
     }
 
     fun getPlanTechOperations(dataId: String): Single<List<PlanTechOperationsModel>> {
         return routesRepository.getPlanTechOperations(dataId)
-            .subscribeOn(Schedulers.io())
+                .subscribeOn(Schedulers.io())
     }
 
     fun getDefectTypical(): Single<List<DefectTypicalModel>> {
@@ -30,6 +30,18 @@ class RoutesInteractor(
 
     fun getEquipments(names: List<String>, uuid: List<String>): Single<List<EquipmentsModel>> {
         return routesRepository.getEquipments(names, uuid)
-            .subscribeOn(Schedulers.io())
+                .subscribeOn(Schedulers.io())
+    }
+
+    fun getDefects(id: String,
+                   codes: List<Int>,
+                   dateDetectStart: String,
+                   dateDetectEnd: String,
+                   detourIds: List<String>,
+                   defectNames: List<String>,
+                   equipmentNames: List<String>,
+                   statusProcessId: String): Single<List<DefectModel>> {
+        return routesRepository.getDefects(id, codes, dateDetectStart, dateDetectEnd, detourIds, defectNames, equipmentNames, statusProcessId)
+                .subscribeOn(Schedulers.io())
     }
 }
