97397def495e8b258951b6fbf842cc2ce9a20d21
Aleksey Polesov <a.polesov@madbrains.ru>
2021-03-04 15:18:43 +0400

    [24] add text button

--- app/src/main/java/ru/madbrains/inspection/ui/delegates/DefectListDelegate.kt
+++ app/src/main/java/ru/madbrains/inspection/ui/delegates/DefectListDelegate.kt
@@ -1,11 +1,16 @@
 package ru.madbrains.inspection.ui.delegates
 
 import android.view.View
+import androidx.core.content.ContentProviderCompat.requireContext
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.CircleCrop
+import com.bumptech.glide.request.RequestOptions
 import com.hannesdorfmann.adapterdelegates4.dsl.adapterDelegateLayoutContainer
 import kotlinx.android.synthetic.main.item_defect.view.*
 import kotlinx.android.synthetic.main.item_defect.view.clContainer
 import ru.madbrains.inspection.R
 import ru.madbrains.inspection.base.model.DiffItem
+import ru.madbrains.inspection.ui.adapters.DefectMediaAdapter
 
 fun defectListDelegate(clickActionLeft: (DefectListUiModel) -> Unit,
                        clickActionRight: (DefectListUiModel) -> Unit) =
@@ -13,14 +18,12 @@ fun defectListDelegate(clickActionLeft: (DefectListUiModel) -> Unit,
 
             bind {
                 itemView.apply {
-
-                    if(item.detour.isEmpty()){
+                    if (item.detour.isEmpty()) {
                         ivIconDetour.setImageResource(R.drawable.ic_defect_card_no_detour)
-                    }
-                    else {
+                    } else {
                         ivIconDetour.setImageResource(R.drawable.ic_defect_card_detour)
                     }
-                        
+
                     tvTitleDate.text = item.date
                     tvTime.text = item.time
                     tvDeviceData.text = item.device
@@ -58,7 +61,8 @@ fun defectListDelegate(clickActionLeft: (DefectListUiModel) -> Unit,
                         clickActionRight.invoke(item)
                     }
 
-                    if(item.isCommonList){
+
+                    if (item.isCommonList) {
                         btnActionLeft.text = resources?.getText(R.string.fragment_btn_label_edit)
                         btnActionRight.text = resources?.getText(R.string.fragment_btn_label_delete)
                     } else {
@@ -66,8 +70,18 @@ fun defectListDelegate(clickActionLeft: (DefectListUiModel) -> Unit,
                         btnActionRight.text = resources?.getText(R.string.fragment_btn_label_fixed)
                     }
 
+                    rvDefectMedia.adapter = DefectMediaAdapter(onMediaDeleteClick = {
+
+                    },
+                            onMediaImageClick = {
+
+                            })
+
+
                 }
             }
+
+
         }
 
 data class DefectListUiModel(
@@ -78,7 +92,8 @@ data class DefectListUiModel(
         val device: String,
         val type: String,
         val description: String,
-        val isCommonList: Boolean
+        val isCommonList: Boolean,
+        val images: List<String>?
 
 ) : DiffItem {
 
--- app/src/main/java/ru/madbrains/inspection/ui/delegates/MediaDefectDelegate.kt
+++ app/src/main/java/ru/madbrains/inspection/ui/delegates/MediaDefectDelegate.kt
@@ -2,6 +2,10 @@ package ru.madbrains.inspection.ui.delegates
 
 import android.graphics.Bitmap
 import android.view.View
+import androidx.core.content.ContentProviderCompat.requireContext
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.CircleCrop
+import com.bumptech.glide.request.RequestOptions
 import com.hannesdorfmann.adapterdelegates4.dsl.adapterDelegateLayoutContainer
 import kotlinx.android.synthetic.main.item_media.view.*
 import ru.madbrains.inspection.R
@@ -25,14 +29,48 @@ fun mediaDefectDelegate(clickImageListener: (MediaDefectUiModel) -> Unit,
                     ivMediaContent.setOnClickListener {
                         clickImageListener.invoke(item)
                     }
+
+                    if(item.isEditing) {
+
+                    } else {
+                        ivMediaDelete.visibility = View.GONE
+
+                    }
+
+
+                    Glide.with(context)
+                        .load(item.url)
+                        .apply(RequestOptions.bitmapTransform(CircleCrop()))
+                        .into(ivMediaContent)
+/*
+                    if(item.url.isNullOrEmpty()){
+
+                        ivMediaContent.setImageBitmap(item.image)
+                    } else {
+                        if (item.isVideo) {
+                            //todo video download and preview
+
+                        } else {
+                            Glide.with(context)
+                                .load(item.url)
+                                .apply(RequestOptions.bitmapTransform(CircleCrop()))
+                                .into(ivMediaContent)
+                        }
+                    }*/
+
                 }
             }
+
+
         }
 
 data class MediaDefectUiModel(
         val id: String,
-        val isVideo: Boolean,
-        val image: Bitmap
+        val isVideo: Boolean = false,
+        val isEditing: Boolean = false,
+        val videoPreview: Bitmap,
+        val image: Bitmap?,
+        val url: String?
 ) : DiffItem {
 
     override fun areItemsTheSame(newItem: DiffItem): Boolean =
