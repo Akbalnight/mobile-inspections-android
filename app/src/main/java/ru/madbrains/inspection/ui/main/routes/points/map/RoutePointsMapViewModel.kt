1134bbed7e3d17958d58a7fb174f6195b90886c5
mad.a.lobachev <a.lobachev@madbrains.ru>
2021-04-07 11:37:12 +0400

    [41] add sorting for map points

--- app/src/main/java/ru/madbrains/inspection/ui/main/routes/points/map/RoutePointsMapViewModel.kt
+++ app/src/main/java/ru/madbrains/inspection/ui/main/routes/points/map/RoutePointsMapViewModel.kt
@@ -4,7 +4,6 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import ru.madbrains.domain.model.DetourModel
 import ru.madbrains.domain.model.RouteDataModel
-import ru.madbrains.domain.model.RouteMapModel
 import ru.madbrains.inspection.base.BaseViewModel
 import ru.madbrains.inspection.base.Event
 import ru.madbrains.inspection.ui.delegates.MapLevelUiModel
@@ -19,20 +18,32 @@ class RoutePointsMapViewModel : BaseViewModel() {
     private val _navigateToTechOperations = MutableLiveData<Event<RouteDataModel>>()
     val navigateToTechOperations: LiveData<Event<RouteDataModel>> = _navigateToTechOperations
 
+    private lateinit var detourModel: DetourModel
+
     fun setData(detour: DetourModel) {
-        _mapLevels.value = detour.route.routeMaps?.mapIndexed { i, map ->
+        detourModel = detour
+        val levels = detour.route.routeMaps?.mapIndexed { i, map ->
             MapLevelUiModel(map.id, map.name, map.url, i == 0)
+        }.also {
+            _mapLevels.value = it
         }
-        _mapPoints.value = detour.route.routesData
+        filterMapPoints(levels?.find { it.isActive })
     }
 
     fun setActiveMap(map: MapLevelUiModel) {
         _mapLevels.value = _mapLevels.value?.map {
             MapLevelUiModel(it.id, it.name, it.url, map.id == it.id)
         }
+        filterMapPoints(map)
+    }
+
+    private fun filterMapPoints(map: MapLevelUiModel?) {
+        if (map == null) return
+
+        _mapPoints.value = detourModel.route.routesData.filter { it.routeMapId == map.id }
     }
 
     fun routePointClick(routeData: RouteDataModel?) {
-        routeData?.let {  _navigateToTechOperations.value = Event(it) }
+        routeData?.let { _navigateToTechOperations.value = Event(it) }
     }
 }
