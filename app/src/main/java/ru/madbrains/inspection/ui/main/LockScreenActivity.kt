032ef90b9b58a0b6f2844f55cd97644f7ec4af32
radroot <radroot@gmail.com>
2021-06-16 19:03:42 +0400

    Revert "[86] seperate fragment"
    This reverts commit 1aa556fc

--- app/src/main/java/ru/madbrains/inspection/ui/main/LockScreenActivity.kt
+++ app/src/main/java/ru/madbrains/inspection/ui/main/LockScreenActivity.kt
@@ -4,13 +4,12 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import androidx.core.view.isInvisible
+import androidx.core.widget.addTextChangedListener
 import androidx.lifecycle.Observer
-import androidx.navigation.NavController
-import androidx.navigation.findNavController
 import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.activity_lock_screen.*
-import kotlinx.android.synthetic.main.activity_lock_screen.progressView
-import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.toolbar_empty.*
 import org.koin.androidx.viewmodel.ext.android.viewModel
 import ru.madbrains.inspection.R
 import ru.madbrains.inspection.base.BaseActivity
@@ -22,8 +21,6 @@ import ru.madbrains.inspection.ui.auth.AuthorizationActivity
 class LockScreenActivity : BaseActivity(R.layout.activity_lock_screen) {
 
     private val lockScreenViewModel: LockScreenViewModel by viewModel()
-    private val syncViewModel: SyncViewModel by viewModel()
-    private lateinit var navController: NavController
 
     companion object {
         fun start(context: Context) {
@@ -34,8 +31,19 @@ class LockScreenActivity : BaseActivity(R.layout.activity_lock_screen) {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        navController = findNavController(R.id.nav_host_container)
-
+        tvTitle.text = strings[R.string.auth]
+        btnLogin.setOnClickListener {
+            lockScreenViewModel.login(tvUsername.text.toString(), tvPassword.text.toString())
+        }
+        btnLogout.setOnClickListener {
+            lockScreenViewModel.logout()
+        }
+        tvUsername.addTextChangedListener {
+            tvValidationError.isInvisible = true
+        }
+        tvPassword.addTextChangedListener {
+            tvValidationError.isInvisible = true
+        }
         lockScreenViewModel.navigateToMain.observe(this, EventObserver {
             MainActivity.start(this)
         })
@@ -43,29 +51,12 @@ class LockScreenActivity : BaseActivity(R.layout.activity_lock_screen) {
             AuthorizationActivity.start(this)
             this.finish()
         })
+        lockScreenViewModel.showError.observe(this, EventObserver {
+            tvValidationError.text = strings[it]
+            tvValidationError.isInvisible = false
+        })
         lockScreenViewModel.progressVisibility.observe(this, Observer {
             progressView.changeVisibility(it)
         })
-        syncViewModel.openSyncDialog.observe(this, EventObserver {
-            navController.navigate(R.id.to_btSyncPanelFragment)
-        })
-        syncViewModel.showSnackBar.observe(this, EventObserver {
-            showSnackBar(resources.getString(it))
-        })
-        syncViewModel.globalProgress.observe(this, Observer {
-            progressView.changeVisibility(it)
-        })
-    }
-
-    private fun showSnackBar(text: String) {
-        val snackBar = Snackbar
-            .make(clLockScreenMain, text, Snackbar.LENGTH_SHORT)
-            .setAction(strings[R.string.proceed], View.OnClickListener {
-
-            })
-        snackBar.setTextColor(colors[R.color.textWhite])
-        snackBar.setActionTextColor(colors[R.color.accidentDark])
-        snackBar.setBackgroundTint(colors[R.color.colorPrimaryDark])
-        snackBar.show()
     }
 }
