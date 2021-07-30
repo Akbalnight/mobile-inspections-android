5ece19655e26a421d833915fd4432174c4848de6
radroot <radroot@gmail.com>
2021-07-30 11:00:39 +0400

    [96] checkpoint dao

---
+++ data/src/main/java/ru/madbrains/data/database/models/CheckpointItemDB.kt
@@ -0,0 +1,13 @@
+package ru.madbrains.data.database.models
+
+import androidx.room.Entity
+import androidx.room.TypeConverters
+
+@Entity(primaryKeys = ["id"])
+@TypeConverters(Converters::class)
+data class CheckpointItemDB(
+    val id: String,
+    val code: Int,
+    val name: String,
+    val rfidCode: String?
+)
--- domain/src/main/java/ru/madbrains/domain/model/CheckpointModel.kt
+++ domain/src/main/java/ru/madbrains/domain/model/CheckpointModel.kt
@@ -2,12 +2,6 @@ package ru.madbrains.domain.model
 
 import java.io.Serializable
 
-data class CheckpointGroupModel(
-    val parentId: String?,
-    val parentName: String?,
-    val points: List<CheckpointModel>
-) : Serializable
-
 data class CheckpointModel(
     val id: String,
     val code: Int,
