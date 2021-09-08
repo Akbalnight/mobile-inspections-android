4d7c408bb497b6fd999bd9bc76889141d456c85d
radroot <radroot@gmail.com>
2021-09-08 17:38:57 +0400

    [59] fix sql

--- data/src/main/java/ru/madbrains/data/database/DetourItemDao.kt
+++ data/src/main/java/ru/madbrains/data/database/DetourItemDao.kt
@@ -20,7 +20,7 @@ interface DetourItemDao {
     @Query("SELECT * FROM DetourItemDB ORDER BY dateStartPlan DESC")
     fun getItems(): Single<List<DetourItemDB>>
 
-    @Query("SELECT r.*, COUNT(d.id) as defectCount FROM DetourItemDB as r LEFT JOIN DefectItemDB as d ON d.detourId = r.id WHERE d.statusProcessId!=:eliminatedId AND d.created = 1 GROUP BY r.id ORDER BY r.dateStartPlan DESC")
+    @Query("SELECT r.*, COUNT(d.id) as defectCount FROM DetourItemDB as r LEFT JOIN DefectItemDB as d ON d.detourId = r.id AND d.statusProcessId!=:eliminatedId AND d.created = 1 GROUP BY r.id ORDER BY r.dateStartPlan DESC")
     fun getItemsWithDefectCount(eliminatedId: String): Single<List<DetourWithDefectCountItemDB>>
 
     @Query("SELECT * FROM DetourItemDB WHERE changed = 1 ORDER BY dateStartPlan DESC")
